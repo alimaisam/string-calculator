338408e11d7027473ad09fdefa33290d2bee4647
Ali Maisam <alimaisam@MoneyMatchs-MacBook-Pro.local>
2021-09-04 12:53:10 +0800

    create calculator object

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 
-function Add(numbers) {
-
-    if (!numbers.length) return 0
+const Calculator = {
+    Add: function(numbers) {
+        if (!numbers.length) return 0
     
-    //split string to array
-    let splitNums = numbers.split(',')
-    
-    //add using map
-    let sum = 0;
-    splitNums.map(num => {
-        return sum += parseInt(num)
-    })
+        //split string to array
+        let splitNums = numbers.split(',')
+        
+        //add using map
+        let sum = 0;
+        splitNums.map(num => {
+            return sum += parseInt(num)
+        })
 
-    return sum
+        return sum
+    }
 }
-
-console.log(Add("1,\n2,4"))
